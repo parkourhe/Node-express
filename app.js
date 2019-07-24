1f9ec2f58656a6a3b347763081e4a83c80ae22a1
parkourhe <995758538@qq.com>
2019-07-24 17:01:18 +0800

    express框架案例

---
+++ app.js
@@ -0,0 +1,66 @@
+
+var express = require('express')
+
+var app = express()
+
+app.engine('html',require('express-art-template'))
+
+//通过node.js可以轻松控制服务器的公共资源和私有资源
+// createServer的函数默认绑定request事件
+
+var comments = [
+	{
+		name:'小明',
+		message:'你好啊aasdasdasdsa',
+		dateTime:'2019/12/12'
+	},
+	{
+		name:'小明',
+		message:'你好啊aasdasdasdsa',
+		dateTime:'2019/12/12'
+	},
+	{
+		name:'小明',
+		message:'你好啊aasdasdasdsa',
+		dateTime:'2019/12/12'
+	},
+	{
+		name:'小明',
+		message:'你好啊aasdasdasdsa',
+		dateTime:'2019/12/12'
+	}
+
+
+]
+app.use('/public/',express.static('./public/'))
+
+
+app.get('/pinglun',function(req,rep){
+	console.log(req.query);
+
+	var data = req.query
+
+	data.dateTime = '2020/9/99'
+
+	comments.unshift(data)
+
+	rep.redirect('/')
+})
+
+
+app.get('/post',function(req,rep){
+	rep.render('post.html')
+})
+
+
+app.get('/',function(req,rep){
+	rep.render('index.html',{
+		comments:comments
+	})
+})
+
+
+
+app.listen('3000',function(){
+	console.log('running...');
+})
